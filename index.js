814b4ee9db87f293b9f0bb939179ccee46fbe942
sharon <sharon.chepkemoi@student.moringaschool.com>
2022-08-23 14:50:40 +0300

    completed assgnment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function writeCards(array, string) {
+    const newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    newArray.push(`Thank you, ${array[i]}, for the wonderful ${string} gift!`);
+  }
+
+  return newArray;
+}
+
+console.log (writeCards(["Guadalupe", "Ollie", "Aki"], "surprise"))
+
+function countDown (number) {
+    let i = number;
+    while (i > -1) {
+        console.log(i--);
+        
+    }
+}
+countDown(10);
